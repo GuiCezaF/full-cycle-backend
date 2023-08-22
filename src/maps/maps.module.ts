d1e619effe8997ab66be1bb8b576f4f17abb5bb6
GuiCezaF <contato.guicezafe@gmail.com>
2023-08-22 16:52:22 +0000

    feat: create route with real cases

--- src/maps/maps.module.ts
+++ src/maps/maps.module.ts
@@ -3,12 +3,15 @@ import { PlacesService } from './places/places.service';
 import { PlacesController } from './places/places.controller';
 import { Client as GoogleMapsClient } from '@googlemaps/google-maps-services-js';
 import { DirectionsController } from './directions/directions.controller';
+import { DirectionsService } from './directions/directions.service';
 
 @Module({
   providers: [
     PlacesService,
     { provide: GoogleMapsClient, useValue: new GoogleMapsClient() },
+    DirectionsService,
   ],
   controllers: [PlacesController, DirectionsController],
+  exports: [DirectionsService],
 })
 export class MapsModule {}
